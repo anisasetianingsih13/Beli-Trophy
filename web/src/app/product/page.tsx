d61e4a859dccb3f8900039f3e69f11ec33c93df1
dias novri <dias_novri_pratama@teknokrat.ac.id>
2025-11-25 21:33:14 +0700

    katalog

--- web/src/app/product/page.tsx
+++ web/src/app/product/page.tsx
@@ -50,7 +50,7 @@ export default function ProdukCatalog() {
 
   return (
     <div className="p-6">
-      <h1 className="text-3xl font-bold mb-6 text-center">Katalog Produk</h1>
+      <h1 className="text-3xl font-bold mb-6 text-center">Katalog Produk thropy</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {products.map((item) => (
