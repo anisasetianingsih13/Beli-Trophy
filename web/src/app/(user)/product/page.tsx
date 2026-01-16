5dc0f73695d0d6b18db24ab8bd66f78706bd7d04
nazwanazliafawaza <nazwa_nazlia_fawaza@teknokrat.ac.id>
2026-01-16 21:14:14 +0700

    Update halaman plakat teks2

--- web/src/app/(user)/product/page.tsx
+++ web/src/app/(user)/product/page.tsx
@@ -36,7 +36,7 @@ export default function ProdukCatalog() {
     },
     {
       id: 5,
-      name: "Trophy Kustom ",
+      name: "Plakat Mini ",
       price: 80000,
       image: "/images/plakat.jpg",
     },
