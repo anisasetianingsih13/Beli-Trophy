e985829c39d9bf92000c5bd18422f0fdff603662
nazwanazliafawaza <nazwa_nazlia_fawaza@teknokrat.ac.id>
2026-01-16 21:33:17 +0700

    Update gambar

--- web/src/app/(user)/product/page.tsx
+++ web/src/app/(user)/product/page.tsx
@@ -56,7 +56,7 @@ export default function ProdukCatalog() {
       id: 8,
       name: "Trophy Penghargaan",
       price: 140000,
-      image: "/images/Wooden.jpg",
+      image: "/images/p1.jpg",
     },
   ];
 
