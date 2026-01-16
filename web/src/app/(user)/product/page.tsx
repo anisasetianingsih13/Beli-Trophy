55d07991db7403850b64b651a6a1c4c8bb395a9a
dias-git <dias_novri_pratama@teknokrat.ac.id>
2026-01-16 22:54:39 +0700

    Tambah gambar

--- web/src/app/(user)/product/page.tsx
+++ web/src/app/(user)/product/page.tsx
@@ -68,7 +68,7 @@ export default function ProdukCatalog() {
       id: 10,
       name: "Trophy FootBall",
       price: 150000,
-      image: "/images/p1.jpg",
+      image: "/images/p2.jpg",
     },
   ];
 
