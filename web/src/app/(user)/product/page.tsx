23f653236bc01a000c4b0360b3e8c1f2c55c2970
dias-git <dias_novri_pratama@teknokrat.ac.id>
2026-01-16 22:55:55 +0700

    Tambah teks

--- web/src/app/(user)/product/page.tsx
+++ web/src/app/(user)/product/page.tsx
@@ -66,7 +66,7 @@ export default function ProdukCatalog() {
     },
     {
       id: 10,
-      name: "Trophy FootBall",
+      name: "Trophy Star",
       price: 150000,
       image: "/images/p2.jpg",
     },
